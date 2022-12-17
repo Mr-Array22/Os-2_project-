9861f3e2c3249095f2e792b9d972615bc4352e5a
mostafa-7ussein <88409894+mostafa-7ussein@users.noreply.github.com>
2022-12-17 18:49:23 +0200

    Add files via upload

---
+++ Mostafa Hussein/Java fx/Producer&Consumer/src/Producer_Consumer_Problem/Producer.java
@@ -0,0 +1,29 @@
+package Producer_Consumer_Problem;
+
+public class Producer implements Runnable {
+	
+	private Container container;
+
+	private boolean exit = false;
+	
+	public Producer(Container container) {
+
+		this.container = container;
+	}
+
+	public void setExit(boolean flag) {
+
+		exit = flag;
+	}
+	@Override
+	public void run() {
+		while (!exit) {
+				try {  
+					container.produce();
+					Thread.sleep(1500);
+				} catch (InterruptedException e) {  
+					e.printStackTrace();  
+				}
+			}
+		}	
+}
